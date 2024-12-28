0e058c92a70d97b1a5f0fd3b99cb5c5932347c79
Vincent De Borger <vincent.deborger@gmail.com>
2024-12-28 15:26:33 +0100

    chore: fill circle

--- pkg/utils/circle.go
+++ pkg/utils/circle.go
@@ -11,15 +11,15 @@ func Circle(img *image.Gray, x0, y0, r int) {
 	err := dx - (r * 2)
 	c := color.Gray{Y: 0}
 
-	for x > y {
-		img.Set(x0+x, y0+y, c)
-		img.Set(x0+y, y0+x, c)
-		img.Set(x0-y, y0+x, c)
-		img.Set(x0-x, y0+y, c)
-		img.Set(x0-x, y0-y, c)
-		img.Set(x0-y, y0-x, c)
-		img.Set(x0+y, y0-x, c)
-		img.Set(x0+x, y0-y, c)
+	for x >= y {
+		for i := x0 - x; i <= x0+x; i++ {
+			img.Set(i, y0+y, c)
+			img.Set(i, y0-y, c)
+		}
+		for i := x0 - y; i <= x0+y; i++ {
+			img.Set(i, y0+x, c)
+			img.Set(i, y0-x, c)
+		}
 
 		if err <= 0 {
 			y++
