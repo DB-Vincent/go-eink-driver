ba6f5ed235cc0eb7806feb85233f97692ad22063
Vincent De Borger <vincent.deborger@gmail.com>
2024-12-28 09:57:58 +0100

    chore: correctly align text

--- pkg/utils/text.go
+++ pkg/utils/text.go
@@ -15,7 +15,7 @@ func Text(s string) image.Image {
 	img := image.NewRGBA(image.Rect(0, 0, 7*len(s)+55, 25))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.White}, image.Point{}, draw.Src)
 
-	point := fixed.Point26_6{X: fixed.I(0), Y: fixed.I(10)}
+	point := fixed.Point26_6{X: fixed.I(0), Y: fixed.I(25)}
 	d := &font.Drawer{
 		Dst:  img,
 		Src:  image.NewUniform(color.Black),
@@ -27,7 +27,7 @@ func Text(s string) image.Image {
 	rotatedImg := image.NewRGBA(image.Rect(0, 0, img.Bounds().Dy(), img.Bounds().Dx()))
 	for x := 0; x < img.Bounds().Dx(); x++ {
 		for y := 0; y < img.Bounds().Dy(); y++ {
-			rotatedImg.Set(y, img.Bounds().Dx()-1-x, img.At(x, y))
+			rotatedImg.Set(img.Bounds().Dy()-1-y, x, img.At(x, y))
 		}
 	}
 	img = rotatedImg
